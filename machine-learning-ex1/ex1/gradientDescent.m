501733b30f43b55c7f15d86b4a00972324815458
MSI\edwin <edwinchen1000@gmail.com>
2018-06-22 00:45:10 -0400

    Finished Week 3 Programming Assignment

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -8,27 +8,12 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
     
     temp0 = theta(1) - sum(X * theta - y) / m * alpha;
     temp1 = theta(2) - sum((X * theta - y) .* X(:, 2)) / m * alpha;
 
     theta(1) = temp0;
     theta(2) = temp1;
-
-
-
-
-
-
-    % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
